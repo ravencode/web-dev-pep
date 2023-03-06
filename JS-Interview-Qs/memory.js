d9d85834c5ec32b9d2331da3a35e6d83c96cc464
Bharat Kharbanda <bharatkharbanda4@gmail.com>
2023-03-06 19:46:51 +0530

    img 2

---
+++ JS-Interview-Qs/memory.js
@@ -0,0 +1,8 @@
+//windows
+// console.log(global)
+
+// node
+// console.log(this)
+
+// the code which is not inside any function is part of global scope
+// the wrapper is called global execution context
